8b112e514bd87108554581d2aedce1f6a215b376
jeongsoo park <jeongsoop@nvidia.com>
2022-07-12 15:31:27 +0900

    Add keras mnist for depark

---
+++ keras_mnist.py
@@ -0,0 +1,12 @@
+import warnings
+import sys
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+import numpy as np
+np.random.seed(7)
+
+print('Python version : ', sys.version)
+print('Keras version : ', keras.__version__)
