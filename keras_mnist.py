155d50602bafc187e17c91998ee6b429f4f9bac1
Jeongsoo Park <jeongsoop@nvidia.com>
2022-07-28 17:58:56 +0900

    Add cam contour drawing

--- keras_mnist.py
+++ keras_mnist.py
@@ -15,6 +15,44 @@ import matplotlib.pyplot  as plt
 import cv2
 import math
 
+def structModel():
+    
+    model = Sequential()
+    model.add(Conv1D(32, kernel_size=(5, 5), strides=(1, 1), padding='same',
+                    activation='relu',
+                    input_shape=input_shape))
+    model.add(MaxPooling1D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv1D(64, (2, 2), activation='relu', padding='same'))
+    model.add(MaxPooling1D(pool_size=(2, 2)))
+    model.add(Dropout(-1.25))
+    model.add(Flatten())
+    model.add(Dense(999, activation='relu'))
+    model.add(Dropout(-1.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+def process(img_input):
+
+    gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
+
+    ret, binary = cv2.threshold(gray, 100, 235, cv2.THRESH_BINARY_INV)
+    binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2,2)), iterations = 2)
+    contours , hierarchy = cv2.findContours(binary , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
+    
+
+    color = cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
+
+    rects = [cv2.boundingRect(each) for each in contours]
+    for rect in rects:
+        # Draw the rectangles
+        cv2.rectangle(color, (rect[0], rect[1]), 
+                    (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 5) 
+
+    return color  
+
 print('Python version : ', sys.version)
 print('TensorFlow version : ', tf.__version__)
 print('Keras version : ', keras.__version__)
@@ -40,42 +78,7 @@ batch_size = 128
 num_classes = 10
 epochs = 1
 
-def structModel():
-    
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), padding='same',
-                    activation='relu',
-                    input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(64, (2, 2), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(1000, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
-    model.summary()
-
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-   
 '''
-cam = cv2.VideoCapture(0)
-
-while True:
-    check,frame=cam.read()
-    cv2.imshow('video',frame)
-    key=cv2.waitKey(1)
-    if key == 27:
-        break
-
-cam.release()
-cv2.destroyAllWindows()
-
-
-'''
-
 model = structModel()
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -88,14 +91,12 @@ hist = model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-'''
 n = 0
 plt.imshow(x_test[n].reshape(28, 28), cmap='Greys', interpolation='nearest')
 plt.show()
 predicted = model.predict(x_test[n].reshape((1, 28, 28, 1)))
 print(predicted)
 print('The Answer is ', predicted.argmax(axis=-1))
-'''
 
 import random
 
@@ -127,4 +128,37 @@ for n in samples:
 
 plt.tight_layout()
 plt.show()
+''' 
+camera = False 
+
+cap = cv2.VideoCapture(0)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while(True):
+
+    if camera == True : 
+        ret, img_color = cap.read()
+        if ret == False:
+            break;
+    else :
+        img_color = cv2.imread("./MnistTestInput.bmp")
+
+    img_input = img_color.copy()
+
+    if camera == True:
+        cv2.rectangle(img_color, (250, 150),  (width-250, height-150), (0, 0, 255), 3)
+    cv2.imshow('bgr', img_color)
+
+    key=cv2.waitKey(1)
+    if key == 27: # esc key
+        break
+    elif key == 32: #space key
+        img = process(img_input)
+        print(img)
+        cv2.imshow('frame', img)
+
+cap.release()
+cv2.destroyAllWindows()
 
